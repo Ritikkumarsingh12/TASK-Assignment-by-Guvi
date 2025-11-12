060623b6c0ccd1e7ac9b6e0b28cbe4389fa6f6b4
RITIK KUMAR SINGH <91491465+Ritikkumarsingh12@users.noreply.github.com>
2025-11-12 21:28:48 +0530

    Update task_1.py

--- TASK 4 By Ritik (programms)/task_1.py
+++ TASK 4 By Ritik (programms)/task_1.py
@@ -1,9 +1,19 @@
               #___Separate Even and Odd Numbers____
 
-numbers_list = [10, 501, 22, 37, 100, 999, 87, 351]
 
-even_list = [value for value in numbers_list if value % 2 == 0]
-odd_list = [value for value in numbers_list if value % 2 != 0]
+#here is the given list of numbers 
+numbers = [10, 501, 22, 37, 100, 999, 87, 351]
 
+#here we have created two empty lists to store even and odd numbers 
+even_list = []
+odd_list = []
+
+for num in numbers:
+    if num % 2 == 0:            #it will check that number is even if devided by 2
+        even_list.append(num)
+    else:
+        odd_list.append(num)
+
+#here it will print even and odd number
 print("Even Numbers:", even_list)
 print("Odd Numbers:", odd_list)
